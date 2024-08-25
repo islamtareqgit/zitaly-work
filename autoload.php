b91c6bd5874a590bf354b04a72f661d8e49f3e3a
tareq <blnre.tareq@gmail.com>
2024-08-25 14:17:36 +0600

    All files including app files are included

---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php 
+date_default_timezone_set('asia/dhaka');
+
+// include functions
+if( file_exists( __DIR__ .'/app/functions.php') ){
+    include_once __DIR__.'/app/functions.php';
+}
+
+// include functions
+if( file_exists( __DIR__ .'/app/data.php') ){
+    include_once __DIR__.'/app/data.php';
+}
+
+
+?>
